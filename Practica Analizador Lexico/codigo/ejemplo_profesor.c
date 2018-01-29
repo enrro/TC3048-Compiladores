f8eb478c86687bf1d6c7c3a197d8577a4f093b41
enrro <edgarbg585@gmail.com>
2018-01-28 19:48:13 -0600

    first commit

---
+++ Practica Analizador Lexico/codigo/ejemplo_profesor.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *inicioLexema, *avance;
+char ultimoLexema[100];
+
+enum token {OPERADOR_RELACION= 0};
+
+void aceptarPalabra() {
+    long inicioPalabra = ftell(inicioLexema);
+    long finPalabra = ftell(avance);
+    fread(ultimoLexema, finPalabra - inicioPalabra, 1, inicioLexema);
+    fseek(inicioLexema, finPalabra, SEEK_SET);
+}
+void rechazarPalabra() {
+    fseek(avance, ftell(inicioLexema), SEEK_SET);
+}
+
+char obtenerSiguienteCaracter(){
+    return fgetc( avance );
+}
+
+int oprel() {
+    int estadoActual = 0;
+    char c = obtenerSiguienteCaracter();
+    while(c != ' '&& c != '\n'&& c != '\t'&& c != EOF&& estadoActual != -1)
+    {
+        switch(estadoActual){
+            case 0:
+            switch(c){
+                case '=':
+                    estadoActual = 1;
+                    break;
+                case '!':
+                    estadoActual = 3;
+                case '>':
+                    estadoActual = 5;
+                case '<':
+                    estadoActual = 7;
+                default:
+                    estadoActual = -1;
+                    break;
+                }
+                break;
+            case 1:
+                if ( c== '=')
+                    estadoActual = 2;
+                else
+                    estadoActual  = -1;
+                break;
+            case 3:
+                estadoActual = (c == '=') ? 4 : -1;
+                break;
+            case 5:
+                estadoActual = ( c== '=') ? 6: -1;
+                break;
+            case 7:
+                estadoActual = (c == '=') ? 8: 1;
+                break;
+            default:
+            estadoActual = -1;
+            break;
+        }
+        c = obtenerSiguienteCaracter();
+    }
+
+    if(estadoActual == 2 || estadoActual == 4 || estadoActual == 5
+        || estadoActual == 6 || estadoActual == 7 || estadoActual == 8){
+            return OPERADOR_RELACION;
+        }
+    return -1;
+}
+
+int isfeof(){
+    char c = obtenerSiguienteCaracter();
+    rechazarPalabra();
+    return c == EOF;
+}
+
+int main(){
+    fopen_s(&inicioLexema, "archivo_fuente", "r");
+    fopen_s(&avance, "archivo_fuente", "r");
+    while(!isfeof()){
+        if(oprel()!= -1){
+            aceptarPalabra();
+            printf("Operador relacion \n");
+        }
+        else
+        {
+            rechazarPalabra;
+        }
+    }
+
+    fclose(inicioLexema);
+    fclose(avance);
+    system("PAUSE");
+    //getc(stdin);
+    return 0;
+}
