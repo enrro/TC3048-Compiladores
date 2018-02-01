f08f7e00a3a1a3f193b1801559685f219109adb1
enrro <edgarbg585@gmail.com>
2018-01-31 22:52:43 -0600

    having problems with profesor example

--- Practica Analizador Lexico/codigo/ejemplo_profesor.c
+++ Practica Analizador Lexico/codigo/ejemplo_profesor.c
@@ -4,97 +4,117 @@
 FILE *inicioLexema, *avance;
 char ultimoLexema[100];
 
-enum token {OPERADOR_RELACION= 0};
+enum token
+{
+    OPERADOR_RELACION = 0
+};
 
-void aceptarPalabra() {
+void aceptarPalabra()
+{
     long inicioPalabra = ftell(inicioLexema);
     long finPalabra = ftell(avance);
+
     fread(ultimoLexema, finPalabra - inicioPalabra, 1, inicioLexema);
+
     fseek(inicioLexema, finPalabra, SEEK_SET);
 }
-void rechazarPalabra() {
+
+void rechazarPalabra()
+{
     fseek(avance, ftell(inicioLexema), SEEK_SET);
 }
 
-char obtenerSiguienteCaracter(){
-    printf("%c", avance);
-    return fgetc( avance );
+char obtenerSiguienteCaracter()
+{
+    return fgetc(avance);
 }
 
-int oprel() {
+int oprel()
+{
     int estadoActual = 0;
     char c = obtenerSiguienteCaracter();
-    while(c != ' '&& c != '\n'&& c != '\t'&& c != EOF&& estadoActual != -1)
+    while (c != ' ' && c != '\n' && c != '\t' && c != EOF && estadoActual != -1)
     {
-        switch(estadoActual){
-            case 0:
-            switch(c){
-                case '=':
-                    estadoActual = 1;
-                    break;
-                case '!':
-                    estadoActual = 3;
-                case '>':
-                    estadoActual = 5;
-                case '<':
-                    estadoActual = 7;
-                default:
-                    estadoActual = -1;
-                    break;
-                }
+        printf("arriba %d\n", estadoActual);
+        switch (estadoActual){
+        case 0:
+            switch (c){
+            case '=':
+                estadoActual = 1;
                 break;
-            case 1:
-                if ( c== '=')
-                    estadoActual = 2;
-                else
-                    estadoActual  = -1;
+            case '!':
+                estadoActual = 3;
                 break;
-            case 3:
-                estadoActual = (c == '=') ? 4 : -1;
+            case '>':
+                estadoActual = 5;
                 break;
-            case 5:
-                estadoActual = ( c== '=') ? 6: -1;
-                break;
-            case 7:
-                estadoActual = (c == '=') ? 8: 1;
+            case '<':
+                estadoActual = 7;
                 break;
             default:
+                estadoActual = -1;
+                break;
+            }
+            break;
+        case 1:
+            if (c == '=')
+                estadoActual = 2;
+            else
+                estadoActual = -1;
+            break;
+        case 3:
+            estadoActual = (c == '=') ? 4 : -1;
+            break;
+        case 5:
+            estadoActual = (c == '=') ? 6 : -1;
+            break;
+        case 7:
+            estadoActual = (c == '=') ? 8 : 1;
+            break;
+        default:
             estadoActual = -1;
             break;
         }
+        printf("abajo %d\n", estadoActual);
         c = obtenerSiguienteCaracter();
+        printf("%d\n",(c != ' ' && c != '\n' && c != '\t' && c != EOF && estadoActual != -1));
     }
 
-    if(estadoActual == 2 || estadoActual == 4 || estadoActual == 5
-        || estadoActual == 6 || estadoActual == 7 || estadoActual == 8){
-            return OPERADOR_RELACION;
-        }
+    if (estadoActual == 2 || estadoActual == 4 || estadoActual == 5 || estadoActual == 6 || estadoActual == 7 || estadoActual == 8)
+    {
+        return OPERADOR_RELACION;
+    }
     return -1;
 }
 
-int isfeof(){
+int isfeof()
+{
     char c = obtenerSiguienteCaracter();
     rechazarPalabra();
     return c == EOF;
 }
 
-int main(){
-    FILE *inicioLexema = fopen("archivo_fuente.txt", "r");
-    FILE *avance = fopen("archivo_fuente.txt", "r");
-    while(!isfeof()){
-        if(oprel()!= -1){
+int main()
+{
+    inicioLexema = fopen("archivo_fuente.txt", "r");
+    avance = fopen("archivo_fuente.txt", "r");
+    while (!isfeof())
+    {
+        if (oprel() != -1)
+        {
             aceptarPalabra();
             printf("Operador relacion \n");
         }
         else
         {
-            rechazarPalabra;
+            rechazarPalabra();
         }
     }
 
     fclose(inicioLexema);
     fclose(avance);
-    system("PAUSE");
+    //system("PAUSE");
+    system("read -p 'Press Enter to continue...' var");
     //getc(stdin);
     return 0;
 }
